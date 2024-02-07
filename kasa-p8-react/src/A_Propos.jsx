4b5d8f3be8c7dec5119adb7487c55b07912d44a6
Zinedine Chami <zinedine.chami@gmail.com>
2024-02-08 00:13:17 +0100

    collapse compnent now functions

--- kasa-p8-react/src/A_Propos.jsx
+++ kasa-p8-react/src/A_Propos.jsx
@@ -1,11 +1,15 @@
 import a_propos_header_img from "./assets/a_propos_header_img.png";
 import Banner from "./components/banner";
 import Footer from "./components/footer";
+import Collapse from "./components/collapse";
 
 function A_Propos() {
   return (
     <>
       <Banner banner_image={a_propos_header_img} />
+      <div className="a_propos_content">
+        <Collapse />
+      </div>
       <Footer />
     </>
   );
---
+++ kasa-p8-react/src/components/collapse.jsx
@@ -0,0 +1,27 @@
+function Collapse(prop) {
+  return (
+    <>
+      <button type="button" className="collapse">
+        button name
+      </button>
+      <div class="collapse_content">
+        <p>collapse content</p>
+      </div>
+    </>
+  );
+}
+
+const collapse = document.querySelector(".collapse");
+const collapse_content = document.querySelector(".collapse_content");
+
+function openCollapse() {
+  if (collapse_content.style.display === "none") {
+    collapse_content.style.display = "block";
+  } else {
+    collapse_content.style.display = "none";
+  }
+}
+
+collapse.addEventListener("click", openCollapse);
+
+export default Collapse;
