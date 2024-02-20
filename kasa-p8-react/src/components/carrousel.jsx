70a1729f261b33046d9272ab79d7d9f5d428e5ae
Zinedine Chami <zinedine.chami@gmail.com>
2024-02-21 00:13:48 +0100

    fixed responsive visual bugs for logement page and header

--- kasa-p8-react/src/components/carrousel.jsx
+++ kasa-p8-react/src/components/carrousel.jsx
@@ -15,6 +15,7 @@ function Carrousel(prop) {
   //   "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-5.jpg",
   // ];
 
+  // ! response blocked by corb bug
   const pictures = [prop.imageUrl];
 
   console.log(pictures);
